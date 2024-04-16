d710586d860fe14891e890c0e2bdc8c57a2aa4b7
Nicholas Kearney <nkearney757@gmail.com>
2024-04-16 14:32:38 +0200

    Add test for getRandNumber

--- src/functions.test.ts
+++ src/functions.test.ts
@@ -1,4 +1,4 @@
-import { getRandomWord } from './helpers';
+import { getRandomWord, getRandomNumber } from './helpers';
 
 const mockWords = [{ category: 'fruit', name: 'apple' }];
 
@@ -6,5 +6,17 @@ describe('selectRandomWord', () => {
   it('should return one item from the array', () => {
     const selectedWord = getRandomWord(mockWords);
     expect(mockWords.includes(selectedWord)).toBeTruthy();
+  });
+});
+
+describe('getRandomNumber', () => {
+  it('should return random integer between 0 and the max number passed to function', () => {
+    const numberOfTests = 15;
+    const maxNumber = 10;
+    for (let i = 0; i < numberOfTests; i++) {
+      const randomNum = getRandomNumber(maxNumber);
+      expect(randomNum).toBeGreaterThanOrEqual(0);
+      expect(randomNum).toBeLessThan(maxNumber);
+    }
   });
 });
